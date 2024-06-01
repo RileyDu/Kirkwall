bf606ef6cb254f583df0856b0cfd49312cb94cfd
Riley DuBord <thelifeofdu@gmail.com>
2024-05-31 19:07:38 -0500

    sidebar UI update

--- src/Frontend/Sidebar/Siderbar.js
+++ src/Frontend/Sidebar/Siderbar.js
@@ -5,16 +5,16 @@ import { useNavigate } from 'react-router-dom';
 const Sidebar = () => {
   const navigate = useNavigate();
   return (
-    <Box bg="black" color="white" w="250px" minH="100vh" p="4">
-      <Box mb="4">Sensor Categories</Box>
-      <Box _hover={{ color: 'green' }} mb="2" onClick={() => navigate('/TempSensors')}>
-        Temperature Sensors
+    <Box bg="black" color="white" w="250px" minH="100vh" p="4" >
+      <Box mb="2" fontWeight="bold" fontSize="lg" textDecoration={'underline'}>Sensor Categories</Box>
+      <Box _hover={{ color: 'green' }} mb="2" onClick={() => navigate('/TempSensors')} >
+        - Temperature Sensors
       </Box>
       <Box _hover={{ color: 'green' }} mb="2">
-        Humidity Sensors
+        - Humidity Sensors
       </Box>
       <Box _hover={{ color: 'green' }} mb="2">
-        Soil Moisture Sensors
+        - Soil Moisture Sensors
       </Box>
     </Box>
   );
