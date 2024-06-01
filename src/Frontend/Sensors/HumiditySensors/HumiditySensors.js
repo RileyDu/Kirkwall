2eece13d51ce6fe8331601eaff7e805ae0d13c01
Riley DuBord <thelifeofdu@gmail.com>
2024-05-31 19:21:08 -0500

    removed unneccesary data fetch, parent does it

--- src/Frontend/Sensors/HumiditySensors/HumiditySensors.js
+++ src/Frontend/Sensors/HumiditySensors/HumiditySensors.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Divider } from '@chakra-ui/react';
 
-export default function HumiditySensors() {
+import ChartWrapper from '../../Charts/ChartWrapper';
+import { BarChart, LineChart } from '../../Charts/Charts';
+
+
+export default function HumiditySensors({ weatherData }) {
    return (
        <Box p="4" width={'100%'} height={'100%'}>
-   <Heading size="xl" textAlign={'center'}>Humidity Sensors</Heading>
+   <Heading size="xl" textAlign={'center'} mb={'4'}>Humidity Sensors</Heading>
+   <ChartWrapper title="Humidity Levels">
+            <BarChart data={weatherData} metric="percent_humidity" />
+          </ChartWrapper>
+          <Divider my={'8'} borderColor="blue.500" borderWidth="4px" />
+          <ChartWrapper title="Humidity Levels">
+            <LineChart data={weatherData} metric="percent_humidity" />
+          </ChartWrapper>
    </Box>
    );
 }
--- src/Frontend/Sensors/TempSensors/TempSensors.js
+++ src/Frontend/Sensors/TempSensors/TempSensors.js
@@ -1,24 +1,10 @@
 import { Box, Divider, Heading } from '@chakra-ui/react';
 import TempBarGraph from '../../Charts/TempBarChart';
 import ChartWrapper from '../../Charts/ChartWrapper';
-import { getWeatherData } from '../../../Backend/Graphql_helper';
 import { BarChart, LineChart } from '../../Charts/Charts';
-import { useEffect, useState } from 'react';
 
-export default function TempSensors() {
-  const [weatherData, setWeatherData] = useState([]);
-  const fetchData = async () => {
-    try {
-      const response = await getWeatherData();
-      setWeatherData(response.data.weather_data);
-    } catch (err) {
-      console.error('Failed to fetch weather data', err);
-    }
-  };
+export default function TempSensors({ weatherData }) {
 
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
     <Box p="4" width={'100%'} height={'100%'}>
       <Heading size="lg" textAlign={'center'} mb={'4'}>
@@ -32,13 +18,12 @@ export default function TempSensors() {
         <BarChart data={weatherData} metric="temperature" />
       </ChartWrapper>
       <Divider my={'8'} borderColor="blue.500" borderWidth="4px" />
+
       {/* <ChartWrapper title="Temperature Levels"> */}
         <TempBarGraph /> 
         {/* Brought this in from the previous Demo, doesnt work as well as it should with the wrapper */}
       {/* </ChartWrapper> */}
-      {/* <ChartWrapper title="Humidity Over Time">
-        <LineChart data={weatherData} metric="humidity" />
-      </ChartWrapper> */}
+      
     </Box>
   );
 }
