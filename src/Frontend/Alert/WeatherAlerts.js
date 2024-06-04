4a9a8180cc5cf05974e592d42f127c259ea200e8
Riley DuBord <thelifeofdu@gmail.com>
2024-06-04 10:08:14 -0500

    The weather alerts dont show unless user is logged in

--- src/Frontend/Alert/WeatherAlerts.js
+++ src/Frontend/Alert/WeatherAlerts.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Flex, Text, Alert, AlertIcon, AlertTitle, AlertDescription, CloseButton, Center } from '@chakra-ui/react';
 import axios from 'axios';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../Backend/Firebase';
 
 function WeatherAlerts() {
   const [alerts, setAlerts] = useState([]);
   const [conditions, setConditions] = useState('');
   const [error, setError] = useState('');
+  const [user] = useAuthState(auth);
+
   
   // Hardcoded zipcode
   const zipcode = '58102';
@@ -29,29 +33,32 @@ function WeatherAlerts() {
   }, []);
 
   return (
+      user ? (
     <Center>
-      <Box p={0} width="100%">
-        {error && (
-          <Alert status="error" mb={0} borderRadius="0" bgColor="red.500" color="white">
-            <AlertIcon />
-            <AlertTitle mr={2}>{error}</AlertTitle>
-            <CloseButton position="absolute" right="8px" top="8px" onClick={() => setError('')} />
-          </Alert>
-        )}
-        <Flex justifyContent="center" alignItems="center" mb={0}>
-          
-          {alerts.map((alert, index) => (
-          <Alert key={index} status="warning" mb={0} borderRadius="0" bgColor="red.500" color="white" width="100%">
+        
+    <Box p={0} width="100%">
+    {error && (
+      <Alert status="error" mb={0} borderRadius="0" bgColor="red.500" color="white">
+      <AlertIcon />
+      <AlertTitle mr={2}>{error}</AlertTitle>
+      <CloseButton position="absolute" right="8px" top="8px" onClick={() => setError('')} />
+      </Alert>
+    )}
+    <Flex justifyContent="center" alignItems="center" mb={0}>
+    
+    {alerts.map((alert, index) => (
+      <Alert key={index} status="warning" mb={0} borderRadius="0" bgColor="red.500" color="white" width="100%">
             <Text fontSize="large" fontWeight="bold" paddingRight={5}>{conditions}</Text>
             <AlertIcon color="white"/>
             <AlertDescription>{alert}</AlertDescription>
           </Alert>
         ))}
-
+        
         </Flex>
         
-      </Box>
-    </Center>
+        </Box>
+        </Center>
+      ) : null
   );
 }
 
