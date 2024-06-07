9564bb9a8f725d5dbe825871b8832fac5306cfae
Riley DuBord <thelifeofdu@gmail.com>
2024-06-06 19:22:14 -0500

    rain sensor page set up

--- src/Frontend/Sensors/RainSensors/RainSensors.js
+++ src/Frontend/Sensors/RainSensors/RainSensors.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Divider } from '@chakra-ui/react';
 
-export default function RainSensors() {
+import ChartWrapper from '../../Charts/ChartWrapper';
+import { BarChart, LineChart } from '../../Charts/Charts';
+
+export default function RainSensors({ weatherData }) {
    return (
        <Box p="4" width={'100%'} height={'100%'}>
-   <Heading size="xl" textAlign={'center'}> Rain Sensors </Heading>
-   </Box>
+           <Heading size="xl" textAlign={'center'} mb={'4'}>Rain Sensors</Heading>
+           <ChartWrapper title="Rain Levels">
+               <BarChart data={weatherData} metric="rainfall_amount" />
+           </ChartWrapper>
+           <Divider my={'8'} borderColor="blue.500" borderWidth="4px" />
+           <ChartWrapper title="Rain Levels">
+               <LineChart data={weatherData} metric="rainfall_amount" />
+           </ChartWrapper>
+       </Box>
    );
 }
