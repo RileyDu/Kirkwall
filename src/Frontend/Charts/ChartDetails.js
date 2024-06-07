e5584dcd37cc92e4b127528fac92e772a6235e95
Riley DuBord <thelifeofdu@gmail.com>
2024-06-07 10:45:34 -0500

    formatting

--- src/Frontend/Charts/ChartDetails.js
+++ src/Frontend/Charts/ChartDetails.js
@@ -1,10 +1,9 @@
 import { Box, Icon, Text, Flex } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { getWeatherData } from '../../Backend/Graphql_helper';
-import { FaPlus, FaMinus } from "react-icons/fa";
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
-
-const getLabelForMetric = (metric) => {
+const getLabelForMetric = metric => {
   switch (metric) {
     case 'temperature':
       return { label: '°F', addSpace: false };
@@ -42,27 +41,30 @@ const ChartDetails = ({ chartType, metric }) => {
     return () => clearInterval(intervalId);
   }, []);
 
-
   const reversedData = [...weatherData].reverse();
 
   const currentData = reversedData.map(data => data[metric]);
 
   const min = currentData.length > 0 ? Math.min(...currentData) : 'N/A';
   const max = currentData.length > 0 ? Math.max(...currentData) : 'N/A';
-  const mostRecentValue = currentData.length > 0 ? currentData[currentData.length - 1] : 'N/A';
+  const mostRecentValue =
+    currentData.length > 0 ? currentData[currentData.length - 1] : 'N/A';
 
-  const calculateTimePeriod = (dataLength) => {
+  const calculateTimePeriod = dataLength => {
     const totalMinutes = dataLength * 5;
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(
+      2,
+      '0'
+    )}`;
   };
 
   const timePeriod = calculateTimePeriod(currentData.length - 1); // Subtract 1 to account for the most recent value
 
   const { label, addSpace } = getLabelForMetric(metric);
 
-  const formatValue = (value) => `${value}${addSpace ? ' ' : ''}${label}`;
+  const formatValue = value => `${value}${addSpace ? ' ' : ''}${label}`;
 
   return (
     <Box>
@@ -70,26 +72,73 @@ const ChartDetails = ({ chartType, metric }) => {
         <Text>Loading...</Text>
       ) : (
         <>
-          <Box display="flex" flexDirection="column" alignItems="center" p={2} border="1px solid" borderColor="#212121" borderRadius="md" boxShadow="md">
-            <Text fontSize="2xl" fontWeight="bold">{formatValue(mostRecentValue)}</Text>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            p={2}
+            border="1px solid"
+            borderColor="#212121"
+            borderRadius="md"
+            boxShadow="md"
+          >
+            <Text fontSize="2xl" fontWeight="bold">
+              {formatValue(mostRecentValue)}
+            </Text>
             <Text>Most Recent Value</Text>
           </Box>
-          <Box display="flex" flexDirection="column" alignItems="center" p={2} border="1px solid" borderColor="#212121" borderRadius="md" boxShadow="md" mt={3}>
-            <Text fontSize="2xl" fontWeight="bold">{formatValue(min)}</Text>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            p={2}
+            border="1px solid"
+            borderColor="#212121"
+            borderRadius="md"
+            boxShadow="md"
+            mt={3}
+          >
+            <Text fontSize="2xl" fontWeight="bold">
+              {formatValue(min)}
+            </Text>
             <Text>Min Value</Text>
           </Box>
-          <Box display="flex" flexDirection="column" alignItems="center" p={2} border="1px solid" borderColor="#212121" borderRadius="md" boxShadow="md" mt={3}>
-            <Text fontSize="2xl" fontWeight="bold">{formatValue(max)}</Text>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            p={2}
+            border="1px solid"
+            borderColor="#212121"
+            borderRadius="md"
+            boxShadow="md"
+            mt={3}
+          >
+            <Text fontSize="2xl" fontWeight="bold">
+              {formatValue(max)}
+            </Text>
             <Text>Max Value</Text>
           </Box>
-          <Box display="flex" flexDirection="column" alignItems="center" p={2} border="1px solid" borderColor="#212121" borderRadius="md" boxShadow="md" mt={3}>
-    <Flex alignItems="center">
-      <Text fontSize="2xl" fontWeight="bold" ml={14}>{timePeriod}</Text>
-      <Icon as={FaPlus} color="green.500" mr={2} ml={2} boxSize={5} />
-      <Icon as={FaMinus} color="red.500" boxSize={5} />
-    </Flex>
-    <Text>Time Period</Text>
-  </Box>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            p={2}
+            border="1px solid"
+            borderColor="#212121"
+            borderRadius="md"
+            boxShadow="md"
+            mt={3}
+          >
+            <Flex alignItems="center">
+              <Text fontSize="2xl" fontWeight="bold" ml={14}>
+                {timePeriod}
+              </Text>
+              <Icon as={FaPlus} color="green.500" mr={2} ml={2} boxSize={5} />
+              <Icon as={FaMinus} color="red.500" boxSize={5} />
+            </Flex>
+            <Text>Time Period</Text>
+          </Box>
         </>
       )}
     </Box>
