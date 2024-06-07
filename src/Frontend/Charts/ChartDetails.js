4d84fa71a4e9de0a1b9d779bbf9cd9a6b8bb7d39
Riley DuBord <thelifeofdu@gmail.com>
2024-06-06 19:17:31 -0500

    the timeperiod that the graph is displaying is now show in the details popover

--- src/Frontend/Charts/ChartDetails.js
+++ src/Frontend/Charts/ChartDetails.js
@@ -27,25 +27,29 @@ const ChartDetails = ({ chartType, metric }) => {
 
   const reversedData = [...weatherData].reverse();
 
-  function capitalizeFirstLetter(word) {
-    if (!word) return word;
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  }
-
-  const chartTitle = capitalizeFirstLetter(chartType);
-
   const currentData = reversedData.map(data => data[metric]);
 
   const min = currentData.length > 0 ? Math.min(...currentData) : 'N/A';
   const max = currentData.length > 0 ? Math.max(...currentData) : 'N/A';
   const mostRecentValue = currentData.length > 0 ? currentData[currentData.length - 1] : 'N/A';
 
+  const calculateTimePeriod = (dataLength) => {
+    const totalMinutes = dataLength * 5;
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+  };
+
+
+
+  const timePeriod = calculateTimePeriod(currentData.length-1); // Subtract 1 to account for the most recent value
+
   return (
     <Box>
       {loading ? (
         <Text>Loading...</Text>
       ) : (
-          <>
+        <>
           <Box display="flex" flexDirection="column" alignItems="center" p={2} border="1px solid" borderColor="#212121" borderRadius="md" boxShadow="md">
             <Text fontSize="2xl" fontWeight="bold">{mostRecentValue}</Text>
             <Text>Most Recent Value</Text>
@@ -57,6 +61,10 @@ const ChartDetails = ({ chartType, metric }) => {
           <Box display="flex" flexDirection="column" alignItems="center" p={2} border="1px solid" borderColor="#212121" borderRadius="md" boxShadow="md" mt={3}>
             <Text fontSize="2xl" fontWeight="bold">{max}</Text>
             <Text>Max Value</Text>
+          </Box>
+          <Box display="flex" flexDirection="column" alignItems="center" p={2} border="1px solid" borderColor="#212121" borderRadius="md" boxShadow="md" mt={3}>
+            <Text fontSize="2xl" fontWeight="bold">{timePeriod}</Text>
+            <Text>Time Period</Text>
           </Box>
         </>
       )}
