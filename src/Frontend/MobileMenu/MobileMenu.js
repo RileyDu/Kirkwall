2f24061c0a9be7b36912f32f084f5e150ddcc3f4
Riley DuBord <thelifeofdu@gmail.com>
2024-06-12 17:07:41 -0500

    nav buttons for hamburger menu now closes it after selection

--- src/Frontend/MobileMenu/MobileMenu.js
+++ src/Frontend/MobileMenu/MobileMenu.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Box, Stack, Button, IconButton, Flex, Avatar, Popover, PopoverTrigger, PopoverContent, PopoverHeader, PopoverCloseButton, PopoverBody } from '@chakra-ui/react';
+import { Box, Stack, Button, Flex } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import { WiThermometer, WiStrongWind, WiRain, WiHumidity } from 'react-icons/wi';
-import { CloseIcon } from '@chakra-ui/icons';
 import Logout from '../AuthComponents/Logout';
 
 const MobileMenu = ({ isOpen, onClose }) => {
   const navigate = useNavigate();
 
   if (!isOpen) return null;
+
+  const handleNavigate = (path) => {
+    navigate(path);
+    onClose();
+  };
 
   return (
     <Box position="fixed" top="0" left="0" width="100%" height="100%" bg="rgba(0, 0, 0, 0.9)" zIndex="1000" color="white">
@@ -18,22 +22,15 @@ const MobileMenu = ({ isOpen, onClose }) => {
           alt="kirkwall logo"
           height="150px"
           width="150px"
-          onClick={() => navigate('/')}
+          onClick={() => handleNavigate('/')}
           style={{ cursor: 'pointer' }}
         />
-        {/* <IconButton
-          icon={<CloseIcon />}
-          onClick={onClose}
-          aria-label="Close Mobile Menu"
-          bg="transparent"
-          color="white"
-        /> */}
       </Flex>
       <Stack spacing="8" p="4">
-        <Button variant="outline" color="white" leftIcon={<WiThermometer size="30" />} onClick={() => navigate('/TempSensors')}>Temperature Sensors</Button>
-        <Button variant="outline" color="white" leftIcon={<WiStrongWind size="30" />} onClick={() => navigate('/WindSensors')}>Wind Sensors</Button>
-        <Button variant="outline" color="white" leftIcon={<WiRain size="30" />} onClick={() => navigate('/RainSensors')}>Rain Sensors</Button>
-        <Button variant="outline" color="white" leftIcon={<WiHumidity size="30" />} onClick={() => navigate('/HumiditySensors')}>Humidity Sensors</Button>
+        <Button variant="outline" color="white" leftIcon={<WiThermometer size="30" />} onClick={() => handleNavigate('/TempSensors')}>Temperature Sensors</Button>
+        <Button variant="outline" color="white" leftIcon={<WiStrongWind size="30" />} onClick={() => handleNavigate('/WindSensors')}>Wind Sensors</Button>
+        <Button variant="outline" color="white" leftIcon={<WiRain size="30" />} onClick={() => handleNavigate('/RainSensors')}>Rain Sensors</Button>
+        <Button variant="outline" color="white" leftIcon={<WiHumidity size="30" />} onClick={() => handleNavigate('/HumiditySensors')}>Humidity Sensors</Button>
         <Logout onLogout={onClose} />
       </Stack>
     </Box>
