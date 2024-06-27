add4fdf4000bb97f161bb2d5f91a8cb2f2c0dc51
Riley DuBord <thelifeofdu@gmail.com>
2024-06-27 10:42:10 -0500

    modal template brought in

---
+++ src/Frontend/Charts/ChartExpandModal.js
@@ -0,0 +1,21 @@
+function ChartExpandModal() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    return (
+      <>
+        <Modal onClose={onClose} size={full} isOpen={isOpen}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Lorem count={2} />
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
