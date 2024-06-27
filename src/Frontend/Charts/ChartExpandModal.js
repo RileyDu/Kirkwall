ffc2cee7b97e9f1bb5391a946dbc133ed41a98b3
Riley DuBord <thelifeofdu@gmail.com>
2024-06-27 12:08:32 -0500

    brought in the mini dashboard to the modal, next chart type selector now

--- src/Frontend/Charts/ChartExpandModal.js
+++ src/Frontend/Charts/ChartExpandModal.js
@@ -1,12 +1,13 @@
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Box } from '@chakra-ui/react';
+import MiniDashboard from './ChartDashboard';
 
-const ChartExpandModal = ({ isOpen, onClose, children, title }) => {
+const ChartExpandModal = ({ isOpen, onClose, children, title, weatherData, metric }) => {
   return (
     <Modal onClose={onClose} size="full" isOpen={isOpen}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader bg={"#212121"} color={"white"} fontSize={"2xl"}>{title}</ModalHeader>
-        <ModalCloseButton size={"lg"} color={"white"}/>
+        <ModalCloseButton size={"lg"} color={"white"} mt={2}/>
         <ModalBody>
             <Box display={"flex"}  justifyContent={"space-between"} my={4}>
                 <Button variant={"pill"}>1H</Button>
@@ -28,9 +29,11 @@ const ChartExpandModal = ({ isOpen, onClose, children, title }) => {
           borderRadius="md"
           boxShadow="md"
           border={"2px solid #fd9801"}
+          mb={4}
         >
           {children}
         </Box>
+        <MiniDashboard metric={metric} weatherData={weatherData}/>
         </ModalBody>
         {/* <ModalFooter>
           <Button onClick={onClose}>Close</Button>
