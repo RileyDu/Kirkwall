57f8b30fea66aff9e5fdbec53b7068fa41ce952f
Riley DuBord <thelifeofdu@gmail.com>
2024-07-01 16:16:23 -0500

    the graphs can now be all adjusted on the x axis individually

--- src/Frontend/Main/Main.js
+++ src/Frontend/Main/Main.js
@@ -7,7 +7,6 @@ import { keyframes } from '@emotion/react';
 import { getWeatherData } from '../../Backend/Graphql_helper';
 
 const MainContent = ({
-  // weatherData,
   isMinimized,
   timePeriod,
   adjustTimePeriod,
@@ -18,15 +17,22 @@ const MainContent = ({
   const [rainfallChartType, setRainfallChartType] = useState('bar');
   const [isReady, setIsReady] = useState(false);
   const [weatherData, setWeatherData] = useState([]);
+  const [tempData, setTempData] = useState(null);
+  const [humidityData, setHumidityData] = useState(null);
+  const [windData, setWindData] = useState(null);
+  const [rainfallData, setRainfallData] = useState(null); 
   const [loading, setLoading] = useState(true);
   const { colorMode } = useColorMode();
-  const [selectedTimePeriod, setSelectedTimePeriod] = useState('3H'); // Default time period
+  const [selectedTimePeriodTemp, setSelectedTimePeriodTemp] = useState('3H');
+  const [selectedTimePeriodHumidity, setSelectedTimePeriodHumidity] = useState('3H');
+  const [selectedTimePeriodWind, setSelectedTimePeriodWind] = useState('3H');
+  const [selectedTimePeriodRainfall, setSelectedTimePeriodRainfall] = useState('3H');
   const [specificChartMetric, setSpecificChartMetric] = useState('all');
-
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        console.log('Fetching data for time period:', timePeriod); // Log the time period
         const response = await getWeatherData('all', timePeriod);
         if (Array.isArray(response.data.weather_data)) {
           setWeatherData(response.data.weather_data);
@@ -53,48 +59,120 @@ const MainContent = ({
     setIsReady(false);
     const timer = setTimeout(() => {
       setIsReady(true);
-    }, 600); // delay to let layout stabilize
+    }, 1000); // delay to let layout stabilize
 
     return () => clearTimeout(timer);
   }, [isMinimized]);
 
   const handleTimePeriodChange = (metric, timePeriod) => {
-    setSelectedTimePeriod(timePeriod);
-    setSpecificChartMetric(metric)
+    console.log('Handle Time Period Change:', metric, timePeriod); // Log metric and timePeriod
+    switch (metric) {
+      case 'temperature':
+        setSelectedTimePeriodTemp(timePeriod);
+        break;
+      case 'percent_humidity':
+        setSelectedTimePeriodHumidity(timePeriod);
+        break;
+      case 'wind_speed':
+        setSelectedTimePeriodWind(timePeriod);
+        break;
+      case 'rainfall':
+        setSelectedTimePeriodRainfall(timePeriod);
+        break;
+      default:
+        break;
+    }
+    setSpecificChartMetric(metric);
+    console.log('Selected specific chart metric:', specificChartMetric); // Log the updated metric
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const limit = determineLimitBasedOnTimePeriod(selectedTimePeriod);
-        const result = await getWeatherData(specificChartMetric, limit);
-        setWeatherData(result.data.weather_data); // Adjust according to your API response structure
+        const limit = determineLimitBasedOnTimePeriod(selectedTimePeriodTemp);
+        console.log('Fetching temperature data with limit:', limit); // Log the limit
+        const result = await getWeatherData('temperature', limit);
+        setTempData(result.data.weather_data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching temperature data:', error);
       }
     };
 
-    fetchData();
-  }, [specificChartMetric, selectedTimePeriod]); // Trigger fetchData when metric or selectedTimePeriod changes
+    if (specificChartMetric === 'temperature') {
+      fetchData();
+    }
+  }, [specificChartMetric, selectedTimePeriodTemp]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const limit = determineLimitBasedOnTimePeriod(selectedTimePeriodHumidity);
+        console.log('Fetching humidity data with limit:', limit); // Log the limit
+        const result = await getWeatherData('humidity', limit);
+        setHumidityData(result.data.weather_data);
+      } catch (error) {
+        console.error('Error fetching humidity data:', error);
+      }
+    };
+
+    if (specificChartMetric === 'percent_humidity') {
+      fetchData();
+    }
+  }, [specificChartMetric, selectedTimePeriodHumidity]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const limit = determineLimitBasedOnTimePeriod(selectedTimePeriodWind);
+        console.log('Fetching wind data with limit:', limit); // Log the limit
+        const result = await getWeatherData('wind', limit);
+        setWindData(result.data.weather_data);
+      } catch (error) {
+        console.error('Error fetching wind data:', error);
+      }
+    };
+
+    if (specificChartMetric === 'wind_speed') {
+      fetchData();
+    }
+  }, [specificChartMetric, selectedTimePeriodWind]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const limit = determineLimitBasedOnTimePeriod(selectedTimePeriodRainfall);
+        console.log('Fetching rainfall data with limit:', limit); // Log the limit
+        const result = await getWeatherData('rain', limit);
+        setRainfallData(result.data.weather_data);
+      } catch (error) {
+        console.error('Error fetching rainfall data:', error);
+      }
+    };
+
+    if (specificChartMetric === 'rainfall') {
+      fetchData();
+    }
+  }, [specificChartMetric, selectedTimePeriodRainfall]);
 
   const determineLimitBasedOnTimePeriod = (timePeriod) => {
+    console.log('Determining limit for time period:', timePeriod); // Log the time period
     switch (timePeriod) {
       case '1H':
-        return 13; // Example limit for 1 hour
+        return 13;
       case '3H':
-        return 37; // Example limit for 3 hours
+        return 37;
       case '6H':
-        return 73; // Example limit for 6 hours
+        return 73;
       case '12H':
-        return 145; // Example limit for 12 hours
+        return 145;
       case '1D':
-        return 289; // Example limit for 1 day
+        return 289;
       case '3D':
-        return 865; // Example limit for 3 days
+        return 865;
       case '1W':
-        return 2017; // Example limit for 1 week
+        return 2017;
       default:
-        return 37; // Default limit
+        return 37;
     }
   };
 
@@ -126,7 +204,7 @@ const MainContent = ({
       color={colorMode === 'light' ? 'black' : 'white'}
       flex="1"
       p="4"
-      pt="74px" // Adjust this value based on your header height
+      pt="74px"
       width="100%"
       minHeight="100vh"
       display="flex"
@@ -141,20 +219,19 @@ const MainContent = ({
           <ChartWrapper
             title="Temperature (°F)"
             onChartChange={handleChartChange(setTempChartType)}
-            weatherData={weatherData}
+            weatherData={tempData || weatherData}
             metric="temperature"
             flex="1"
             timePeriod={timePeriod}
-            // adjustTimePeriod={adjustTimePeriod}
             display="flex"
             flexDirection="column"
-            key={tempChartType} // Add this key to ensure re-render
+            key={tempChartType}
             handleTimePeriodChange={handleTimePeriodChange}
           >
             {tempChartType === 'line' ? (
-              <LineChart data={weatherData} metric="temperature" style={{ flex: 1 }} />
+              <LineChart data={tempData || weatherData} metric="temperature" style={{ flex: 1 }} />
             ) : (
-              <BarChart data={weatherData} metric="temperature" style={{ flex: 1 }} />
+              <BarChart data={tempData || weatherData} metric="temperature" style={{ flex: 1 }} />
             )}
           </ChartWrapper>
         </GridItem>
@@ -162,62 +239,59 @@ const MainContent = ({
           <ChartWrapper
             title="Humidity (%)"
             onChartChange={handleChartChange(setHumidityChartType)}
-            weatherData={weatherData}
+            weatherData={humidityData || weatherData}
             metric="percent_humidity"
             flex="1"
             timePeriod={timePeriod}
-            // adjustTimePeriod={adjustTimePeriod}
             display="flex"
             flexDirection="column"
-            key={humidityChartType} // Add this key to ensure re-render
+            key={humidityChartType}
             handleTimePeriodChange={handleTimePeriodChange}
           >
             {humidityChartType === 'line' ? (
-              <LineChart data={weatherData} metric="percent_humidity" style={{ flex: 1 }} />
+              <LineChart data={humidityData || weatherData} metric="percent_humidity" style={{ flex: 1 }} />
             ) : (
-              <BarChart data={weatherData} metric="percent_humidity" style={{ flex: 1 }} />
+              <BarChart data={humidityData || weatherData} metric="percent_humidity" style={{ flex: 1 }} />
             )}
           </ChartWrapper>
         </GridItem>
         <GridItem colSpan={{ base: 1, lg: 2 }} display="flex">
           <ChartWrapper
-            title="Rainfall (inches)"
-            onChartChange={handleChartChange(setRainfallChartType)}
-            weatherData={weatherData}
-            metric="rain_15_min_inches"
-            flex="1"
-            timePeriod={timePeriod}
-            // adjustTimePeriod={adjustTimePeriod}
-            display="flex"
-            flexDirection="column"
-            key={rainfallChartType} // Add this key to ensure re-render
-            handleTimePeriodChange={handleTimePeriodChange}
-          >
-            {rainfallChartType === 'line' ? (
-              <LineChart data={weatherData} metric="rain_15_min_inches" style={{ flex: 1 }} />
-            ) : (
-              <BarChart data={weatherData} metric="rain_15_min_inches" style={{ flex: 1 }} />
-            )}
-          </ChartWrapper>
-        </GridItem>
-        <GridItem colSpan={{ base: 1, lg: 2 }} display="flex">
-          <ChartWrapper
-            title="Wind Speed (MPH)"
+            title="Wind Speed (mph)"
             onChartChange={handleChartChange(setWindChartType)}
-            weatherData={weatherData}
+            weatherData={windData || weatherData}
             metric="wind_speed"
             flex="1"
             timePeriod={timePeriod}
-            adjustTimePeriod={adjustTimePeriod}
             display="flex"
             flexDirection="column"
-            key={windChartType} // Add this key to ensure re-render
+            key={windChartType}
             handleTimePeriodChange={handleTimePeriodChange}
           >
             {windChartType === 'line' ? (
-              <LineChart data={weatherData} metric="wind_speed" style={{ flex: 1 }} />
+              <LineChart data={windData || weatherData} metric="wind_speed" style={{ flex: 1 }} />
             ) : (
-              <BarChart data={weatherData} metric="wind_speed" style={{ flex: 1 }} />
+              <BarChart data={windData || weatherData} metric="wind_speed" style={{ flex: 1 }} />
+            )}
+          </ChartWrapper>
+        </GridItem>
+        <GridItem colSpan={{ base: 1, lg: 2 }} display="flex">
+          <ChartWrapper
+            title="Rainfall (in)"
+            onChartChange={handleChartChange(setRainfallChartType)}
+            weatherData={rainfallData || weatherData}
+            metric="rainfall"
+            flex="1"
+            timePeriod={timePeriod}
+            display="flex"
+            flexDirection="column"
+            key={rainfallChartType}
+            handleTimePeriodChange={handleTimePeriodChange}
+          >
+            {rainfallChartType === 'line' ? (
+              <LineChart data={rainfallData || weatherData} metric="rainfall" style={{ flex: 1 }} />
+            ) : (
+              <BarChart data={rainfallData || weatherData} metric="rainfall" style={{ flex: 1 }} />
             )}
           </ChartWrapper>
         </GridItem>
