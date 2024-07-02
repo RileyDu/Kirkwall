505bdcd9cd42d245e1783b1d690cbde0a428adaa
Riley DuBord <thelifeofdu@gmail.com>
2024-07-02 14:51:35 -0500

    fixed the temp sensors page

--- src/Frontend/Sensors/TempSensors/TempSensors.js
+++ src/Frontend/Sensors/TempSensors/TempSensors.js
@@ -2,10 +2,13 @@ import { Box, Divider, Heading, Flex, Spinner, Text } from '@chakra-ui/react';
 import ChartWrapper from '../../Charts/ChartWrapper';
 import { BarChart, LineChart } from '../../Charts/Charts';
 import MiniDashboard from '../../Charts/ChartDashboard';
+import { useWeatherData } from '../../WeatherDataContext';
 
-export default function TempSensors({ weatherData }) {
+export default function TempSensors({  }) {
 
-  if (!weatherData) {
+  const { weatherData, tempData } = useWeatherData();
+
+  if (!weatherData && !tempData) {
     console.log('WeatherData is not defined', weatherData);
     return (
       <Box p="4" width="100%" height="100%" textAlign="center">
@@ -21,16 +24,16 @@ export default function TempSensors({ weatherData }) {
         Temperature Sensors
       </Heading>
       <Box width="100%">
-        <MiniDashboard weatherData={weatherData} metric="temperature" />
+        <MiniDashboard weatherData={tempData || weatherData} metric="temperature" />
       </Box>
       <Flex direction="row" justifyContent="space-between">
         <Box width="100%">
-          <ChartWrapper title="Temperature (°F)">
-            <LineChart data={weatherData} metric="temperature" />
+          <ChartWrapper title="Temperature (°F)" weatherData={tempData || weatherData}>
+            <LineChart data={tempData || weatherData} metric="temperature" />
           </ChartWrapper>
           <Divider my={'8'} borderColor="#212121" borderWidth="4px" borderRadius={"full"} />
-          <ChartWrapper title="Temperature (°F)">
-            <BarChart data={weatherData} metric="temperature" />
+          <ChartWrapper title="Temperature (°F)" weatherData={tempData || weatherData}>
+            <BarChart data={ tempData || weatherData} metric="temperature" />
           </ChartWrapper>
         </Box>
       </Flex>
