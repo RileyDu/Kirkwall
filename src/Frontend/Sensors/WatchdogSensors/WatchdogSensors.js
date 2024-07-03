7199e0ad877db8fbf3f5b02915660591f9d8b5b2
Riley DuBord <thelifeofdu@gmail.com>
2024-07-03 10:19:29 -0500

    added a loading spinner to watchdog sensor page

--- src/Frontend/Sensors/WatchdogSensors/WatchdogSensors.js
+++ src/Frontend/Sensors/WatchdogSensors/WatchdogSensors.js
@@ -12,14 +12,36 @@ import { useWeatherData } from '../../WeatherDataContext';
 import ChartWrapper from '../../Charts/ChartWrapper';
 import { BarChart, LineChart } from '../../Charts/Charts';
 import MiniDashboard from '../../Charts/ChartDashboard';
+import { FaChessRook } from 'react-icons/fa';
+import { keyframes } from '@emotion/react';
+import { useEffect, useState } from 'react';
 
 export default function WatchdogSensors() {
-  const { watchdogData, isLoading } = useWeatherData();
+  const { watchdogData, loading } = useWeatherData();
+  const [isReady, setIsReady] = useState(false);
 
-  if (isLoading) {
+
+  useEffect(() => {
+    setIsReady(false);
+    if (watchdogData.length > 0) {
+      setIsReady(true);
+    }
+  }, [watchdogData]);
+
+  const spin = keyframes`
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  `;
+
+  if (loading) {
     return (
-      <Flex justifyContent="center" alignItems="center" height="100vh">
-        <Spinner size="xl" />
+      <Flex justify="center" align="center" height="100%">
+        <Box
+          as={FaChessRook}
+          animation={`${spin} infinite 2s linear`}
+          fontSize="6xl"
+          color="black"
+        />
       </Flex>
     );
   }
