b3c872297dd7518385cb98e12c56025875043ed8
Riley DuBord <thelifeofdu@gmail.com>
2024-07-03 13:25:38 -0500

    default grabbing 3 hours for watchdog data, but the data needs to exist for time period modal to look properly

--- src/Frontend/WeatherDataContext.js
+++ src/Frontend/WeatherDataContext.js
@@ -57,7 +57,7 @@ export const WeatherDataProvider = ({ children }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getWatchdogData('all', '13');
+        const response = await getWatchdogData('all', '19');
         if (Array.isArray(response.data.watchdog_data)) {
           setWatchdogData(response.data.watchdog_data);
         } else {
