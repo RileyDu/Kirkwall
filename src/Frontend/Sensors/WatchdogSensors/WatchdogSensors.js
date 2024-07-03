51e6d025de3cacfabe0dc411233c3e5ec2a509d9
Riley DuBord <thelifeofdu@gmail.com>
2024-07-03 01:08:22 -0500

    styled watchdog graphs and cards

--- src/Frontend/Sensors/WatchdogSensors/WatchdogSensors.js
+++ src/Frontend/Sensors/WatchdogSensors/WatchdogSensors.js
@@ -1,8 +1,7 @@
-import { Box, Divider, Heading, Flex, Spinner, Text } from '@chakra-ui/react';
+import { Box, Divider, Heading, Flex, Spinner, Text, Grid, GridItem } from '@chakra-ui/react';
 import { useWeatherData } from '../../WeatherDataContext';
 import ChartWrapper from '../../Charts/ChartWrapper';
 import { BarChart, LineChart } from '../../Charts/Charts';
-// import MiniDashboard from '../../Charts/ChartDashboard';
 
 export default function WatchdogSensors() {
     const { watchdogData, isLoading } = useWeatherData();
@@ -21,30 +20,34 @@ export default function WatchdogSensors() {
                 Watchdog Sensors
             </Heading>
             <Flex direction="row" justifyContent="space-between">
-        <Box width="100%">
-          <ChartWrapper title="Temperature (°F)" weatherData={watchdogData} metric={'temp'}>
-            <LineChart data={watchdogData} metric="temp" />
-          </ChartWrapper>
-          <Divider my={'8'} borderColor="#212121" borderWidth="4px" borderRadius={"full"} />
-          <ChartWrapper title="Humidity (%)" weatherData={watchdogData} metric={'hum'}>
-            <BarChart data={watchdogData} metric="hum" />
-          </ChartWrapper>
-        </Box>
-      </Flex>
-            {watchdogData.map((entry) => (
-                <Box key={entry.dataid} borderWidth="1px" borderRadius="lg" overflow="hidden" p="6" mb="4">
-                    <Heading size="md" mb="2">
-                        Device Location: {entry.device_location}
-                    </Heading>
-                    <Text><strong>Temperature:</strong> {entry.temp}°F</Text>
-                    <Text><strong>Humidity:</strong> {entry.hum}%</Text>
-                    <Text><strong>Water Level:</strong> {entry.water}</Text>
-                    <Text><strong>Reading Time:</strong> {new Date(entry.reading_time).toLocaleTimeString()}</Text>
-                    <Divider my="4" />
-                    <Text><strong>API Name:</strong> {entry.api.apiname}</Text>
-                    <Text><strong>Customer Name:</strong> {entry.api.customer.name}</Text>
+                <Box width="100%">
+                    <ChartWrapper title="Temperature in Garage (°F)" weatherData={watchdogData} metric="temp">
+                        <LineChart data={watchdogData} metric="temp" />
+                    </ChartWrapper>
+                    <Divider my="8" borderColor="#212121" borderWidth="4px" borderRadius="full" />
+                    <ChartWrapper title="Humidity in Garage (%)" weatherData={watchdogData} metric="hum">
+                        <BarChart data={watchdogData} metric="hum" />
+                    </ChartWrapper>
                 </Box>
-            ))}
+            </Flex>
+            <Grid templateColumns="repeat(auto-fill, minmax(300px, 1fr))" gap={6} mt={8}>
+                {watchdogData.map((entry) => (
+                    <GridItem key={entry.dataid}>
+                        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p="6" bg={'brand.50'}>
+                            <Heading size="lg" mb="2">
+                                 Location: {entry.device_location}
+                            </Heading>
+                            <Text><strong>Temperature:</strong> {entry.temp}°F</Text>
+                            <Text><strong>Humidity:</strong> {entry.hum}%</Text>
+                            <Text><strong>Water Level:</strong> {entry.water}</Text>
+                            <Text><strong>Reading Time:</strong> {new Date(entry.reading_time).toLocaleTimeString()}</Text>
+                            <Divider my="4" />
+                            <Text><strong>API Name:</strong> {entry.api.apiname}</Text>
+                            <Text><strong>Customer Name:</strong> {entry.api.customer.name}</Text>
+                        </Box>
+                    </GridItem>
+                ))}
+            </Grid>
         </Box>
     );
 }
