49a2da65b2568eac968da403fe6da5c9c3d58a99
Riley DuBord <thelifeofdu@gmail.com>
2024-07-05 22:15:50 -0500

    styled map for sensor locations

--- src/Frontend/Maps/Map.js
+++ src/Frontend/Maps/Map.js
@@ -1,35 +1,64 @@
 // src/MapComponent.js
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { useState } from 'react';
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from '@vis.gl/react-google-maps';
+import { Heading, Box } from '@chakra-ui/react';
 
-const containerStyle = {
-  width: '100%',
-  height: '400px',
+const mapStyles = {
+  width: '60%',
+  height: '800px',
+  margin: '0 auto',
+  borderRadius: '10px',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+  border: '3px solid #212121',
 };
 
 const center = {
-  lat: 39.8283,
-  lng: -98.5795, // Center of the US
+  lat: 46.877186,
+  lng: -96.789803,
 };
 
 const locations = [
-  { lat: 34.0522, lng: -118.2437 }, // Los Angeles
-  { lat: 40.7128, lng: -74.0060 },  // New York
-  { lat: 41.8781, lng: -87.6298 },  // Chicago
-  // Add more locations as needed
+  { lat: 46.948580, lng: -97.262730, info: 'Grand Farm' },
+  { lat: 46.904340, lng: -96.810500, info: 'Incubator (Garage)' },
 ];
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+const mapID = process.env.REACT_APP_GOOGLE_MAPS_MAP_ID;
 
 const MapComponent = () => {
+  const [openInfoIndex, setOpenInfoIndex] = useState(null);
+
   return (
-    <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={4}>
-        {locations.map((location, index) => (
-          <Marker key={index} position={location} />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <APIProvider apiKey={apiKey}>
+      <Box width="100%" textAlign="center" p={'4'} pt={'64px'}>
+        <Heading size="xl" pb={'4'}>
+          Sensor Map
+        </Heading>
+      </Box>
+      <div className="map-container" style={mapStyles}>
+        <Map defaultCenter={center} defaultZoom={10} mapId={mapID} borderRadius="10px">
+          {locations.map((location, index) => (
+            <AdvancedMarker
+              key={index}
+              position={{ lat: location.lat, lng: location.lng }}
+              onClick={() => setOpenInfoIndex(index)}
+            //   icon=
+            >
+              <Pin background={'#fd9801'} borderColor={'#212121'} glyphColor={'#212121'} />
+              {openInfoIndex === index && (
+                <InfoWindow
+                  position={{ lat: location.lat, lng: location.lng }}
+                  onCloseClick={() => setOpenInfoIndex(null)}
+                  background={'#fd9801'}
+                >
+                  <div style={{ color: '#212121', fontWeight: 'bold', fontSize: '16px' }}>{location.info}</div>
+                </InfoWindow>
+              )}
+            </AdvancedMarker>
+          ))}
+        </Map>
+      </div>
+    </APIProvider>
   );
 };
 
