61b7ae7295f2186c1374950ac9432268924f9ab2
Riley DuBord <thelifeofdu@gmail.com>
2024-07-09 16:09:21 -0500

    trying to get twilio/sendgrid servers up on vercel

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use(cors());
 
 // Twilio credentials from environment variables
