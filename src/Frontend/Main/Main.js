515cef612079a3d1be60baa811b4225343243048
Riley DuBord <thelifeofdu@gmail.com>
2024-07-29 13:52:56 -0500

    component clean up

--- src/Frontend/Main/Main.js
+++ src/Frontend/Main/Main.js
@@ -16,25 +16,19 @@ import {
   TabPanels,
   Tab,
   TabPanel,
-  useColorModeValue,
   IconButton,
   Tooltip,
   Divider,
   useMediaQuery,
   useDisclosure,
 } from '@chakra-ui/react';
-// import VoiceControl from '../services/VoiceControl';
 import { LineChart, BarChart } from '../Charts/Charts.js';
-// import Logout from '../../Frontend/AuthComponents/Logout';
-import { auth } from '../../Backend/Firebase.js';
 import ChartWrapper from '../Charts/ChartWrapper.js';
 import { FaChessRook, FaChevronDown, FaPlus, FaMinus, FaTemperatureHigh, FaTint, FaWind, FaWater, FaLeaf, FaCloudRain } from 'react-icons/fa/index.esm.js';
 import { keyframes } from '@emotion/react';
 import { useWeatherData } from '../WeatherDataContext.js';
 import { handleChartChange } from '../Charts/ChartUtils.js';
 import { motion } from 'framer-motion';
-// import { useAuth } from '../AuthComponents/AuthContext.js';
-// import ChartExpandModal from '../Charts/ChartExpandModal';
 
 const MotionBox = motion(Box);
 const MotionTabPanel = motion(TabPanel);
@@ -84,101 +78,10 @@ const MainContent = ({ timePeriod, statusOfAlerts }) => {
     garage: ['temperature', 'humidity'],
     rivercity: ['temperature', 'humidity'],
   });
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalChart, setModalChart] = useState('');
-  const [currentTimePeriod, setCurrentTimePeriod] = useState('1H');
+
   const { colorMode } = useColorMode();
-  const iconColor = useColorModeValue('black', 'white');
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isLargerThan768] = useMediaQuery('(min-width: 768px)');
-
-  const showChart = (section, chart) => {
-    setVisibleCharts(prevState => ({
-      ...prevState,
-      [section]: [...prevState[section], chart],
-    }));
-  };
-
-  const hideChart = (section, chart) => {
-    setVisibleCharts(prevState => ({
-      ...prevState,
-      [section]: prevState[section].filter(item => item !== chart),
-    }));
-  };
-
-  // const handleVoiceCommand = (command) => {
-  //   if (command.includes('show grand farm')) {
-  //     showChart('grandFarm', 'temperature');
-  //   } else if (command.includes('hide temperature')) {
-  //     hideChart('grandFarm', 'temperature');
-  //   } else if (command.includes('show humidity')) {
-  //     showChart('grandFarm', 'humidity');
-  //   } else if (command.includes('hide humidity')) {
-  //     hideChart('grandFarm', 'humidity');
-  //   } else if (command.includes('show wind')) {
-  //     showChart('grandFarm', 'wind');
-  //   } else if (command.includes('hide wind')) {
-  //     hideChart('grandFarm', 'wind');
-  //   } else if (command.includes('show soil moisture')) {
-  //     showChart('grandFarm', 'soilMoisture');
-  //   } else if (command.includes('hide soil moisture')) {
-  //     hideChart('grandFarm', 'soilMoisture');
-  //   } else if (command.includes('show leaf wetness')) {
-  //     showChart('grandFarm', 'leafWetness');
-  //   } else if (command.includes('hide leaf wetness')) {
-  //     hideChart('grandFarm', 'leafWetness');
-  //   } else if (command.includes('show rainfall')) {
-  //     showChart('grandFarm', 'rainfall');
-  //   } else if (command.includes('hide rainfall')) {
-  //     hideChart('grandFarm', 'rainfall');
-  //   } else if (command.includes('expand temperature details')) {
-  //     openModal('temperature');
-  //   } else if (command.includes('expand humidity details')) {
-  //     openModal('percent_humidity');
-  //   } else if (command.includes('expand wind details')) {
-  //     openModal('wind_speed');
-  //   } else if (command.includes('expand soil moisture details')) {
-  //     openModal('soil_moisture');
-  //   } else if (command.includes('expand leaf wetness details')) {
-  //     openModal('leaf_wetness');
-  //   } else if (command.includes('expand rainfall details')) {
-  //     openModal('rain_15_min_inches');
-  //   } else if (command.includes('log out')) {
-  //     logOut();
-  //   } else if (command.includes('change temperature chart type to line')) {
-  //     setTempChartType('line');
-  //   } else if (command.includes('change temperature chart type to bar')) {
-  //     setTempChartType('bar');
-  //   } else if (command.includes('change humidity chart type to line')) {
-  //     setHumidityChartType('line');
-  //   } else if (command.includes('change humidity chart type to bar')) {
-  //     setHumidityChartType('bar');
-  //   } else {
-  //     console.log('Command not recognized');
-  //   }
-  // };
-
-  const openModal = chart => {
-    setModalChart(chart);
-    setIsModalOpen(true);
-  };
-
-  const logOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        console.log('User logged out');
-        // Additional logout logic if needed
-      })
-      .catch(error => {
-        console.error('Logout error:', error);
-      });
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setModalChart('');
-  };
 
   useEffect(() => {
     setIsReady(false);
@@ -186,13 +89,6 @@ const MainContent = ({ timePeriod, statusOfAlerts }) => {
       setIsReady(true);
     }
   }, [weatherData]);
-
-  // const toggleSection = (section) => {
-  //   setShowSections((prevState) => ({
-  //     ...prevState,
-  //     [section]: !prevState[section],
-  //   }));
-  // };
 
   const toggleChartVisibility = (section, chart) => {
     setVisibleCharts(prevState => {
@@ -227,21 +123,6 @@ const MainContent = ({ timePeriod, statusOfAlerts }) => {
       </Flex>
     );
   }
-
-  const ChartToggleButton = ({ isVisible, onClick, chart, icon }) => (
-    <Tooltip label={isVisible ? `Hide ${chart}` : `Show ${chart}`}>
-      <IconButton
-        icon={isVisible ? <FaMinus /> : <FaPlus />}
-        onClick={onClick}
-        mx="1"
-        bg={isVisible ? 'red.400' : 'green.400'}
-        color="white"
-        _hover={{ bg: isVisible ? 'red.500' : 'green.500' }}
-        size="sm"
-        aria-label={`${isVisible ? 'Hide' : 'Show'} ${chart}`}
-      />
-    </Tooltip>
-  );
 
   const charts = {
     temperature: <FaTemperatureHigh />,
