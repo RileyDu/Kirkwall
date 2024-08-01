3b4687d28d570f0277364076df5e572a58dd23f3
Riley DuBord <thelifeofdu@gmail.com>
2024-08-01 09:31:27 -0500

    chore: Add initial customer data to CustomerSettings.js, need to bring in metrics

---
+++ src/Frontend/Modular/CustomerSettings.js
@@ -0,0 +1,46 @@
+// customers.js
+export const customers = [
+    {
+        id:1,
+        name:"Kirkwall",
+        email:"test@kirkwall.io",
+        phone:"",
+        metric:["", "", "", "", "", ""]
+    },
+    {
+      id: 2,
+      name: "Grand Farm",
+      email: "pmo@grandfarm.com",
+      phone: "",
+      metric: ["", "", "", "", "", ""],
+    },
+    {
+      id: 3,
+      name: "Watchdog Protect",
+      emails: "trey@watchdogprotect.com",
+      phones: "987-654-3210",
+      metric: ["", ""],
+    },
+    {
+      id: 4,
+      name: "Rivercity",
+      email: "",
+      phone: "",
+      metric: ["", ""],
+    },
+    {
+        id: 5,
+        name: "RJ Energy",
+        email: "russell@rjenergysolutions.com",
+        phone: "",
+        metric: ["", ""],
+      },
+      {
+        id: 6,
+        name: "ImpriMed",
+        email: "jerrycromarty@imprimedicine.com",
+        phone: "",
+        metric: ["", "", "", "", "","", "", "", "", "",],
+      },
+  ];
+  
