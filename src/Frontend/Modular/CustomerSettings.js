87c43790df81faa90fe4ea7a51385376ef95e5d0
Riley DuBord <thelifeofdu@gmail.com>
2024-08-01 09:40:46 -0500

    got the metrics for each user in

--- src/Frontend/Modular/CustomerSettings.js
+++ src/Frontend/Modular/CustomerSettings.js
@@ -5,42 +5,42 @@ export const customers = [
         name:"Kirkwall",
         email:"test@kirkwall.io",
         phone:"",
-        metric:["", "", "", "", "", ""]
+        metric:["temperature", "percent_humidity", "wind_speed", "rain_15_min_inches", "soil_moisture", "leaf_wetness", "temp", "hum", "rctemp", "humidity"]
     },
     {
       id: 2,
       name: "Grand Farm",
       email: "pmo@grandfarm.com",
       phone: "",
-      metric: ["", "", "", "", "", ""],
+      metric: ["temperature", "percent_humidity", "wind_speed", "rain_15_min_inches", "soil_moisture", "leaf_wetness",],
     },
     {
       id: 3,
       name: "Watchdog Protect",
       emails: "trey@watchdogprotect.com",
-      phones: "987-654-3210",
-      metric: ["", ""],
+      phones: "",
+      metric: ["temp", "hum",],
     },
-    {
-      id: 4,
-      name: "Rivercity",
-      email: "",
-      phone: "",
-      metric: ["", ""],
-    },
-    {
-        id: 5,
-        name: "RJ Energy",
-        email: "russell@rjenergysolutions.com",
-        phone: "",
-        metric: ["", ""],
-      },
-      {
-        id: 6,
-        name: "ImpriMed",
-        email: "jerrycromarty@imprimedicine.com",
-        phone: "",
-        metric: ["", "", "", "", "","", "", "", "", "",],
-      },
+    // {
+    //   id: 4,
+    //   name: "Rivercity",
+    //   email: "",
+    //   phone: "",
+    //   metric: ["rctemp", "humidity"],
+    // },
+    // {
+    //     id: 5,
+    //     name: "RJ Energy",
+    //     email: "russell@rjenergysolutions.com",
+    //     phone: "",
+    //     metric: ["", ""],
+    //   },
+    //   {
+    //     id: 6,
+    //     name: "ImpriMed",
+    //     email: "jerrycromarty@imprimedicine.com",
+    //     phone: "",
+    //     metric: ["", "", "", "", "","", "", "", "", "",],
+    //   },
   ];
   
