b0e041c3b60e7d17ec6d8a5c9ecc87c823154867
Riley DuBord <thelifeofdu@gmail.com>
2024-08-20 14:32:29 -0500

    changed icon

--- src/Frontend/Charts/ChartWrapper.js
+++ src/Frontend/Charts/ChartWrapper.js
@@ -25,6 +25,7 @@ import {
   FaChartBar,
   FaChartLine,
   FaMap,
+  FaEyeSlash,
 } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
@@ -634,9 +635,9 @@ const ChartWrapper = ({
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 1, delay: 0.5 }}
             >
-              <Tooltip label="Close Chart">
+              <Tooltip label="Hide Chart">
                 <MotionIconButton
-                  icon={<FaTimes />}
+                  icon={<FaEyeSlash />}
                   variant="outline"
                   color="#212121"
                   size={iconSize}
