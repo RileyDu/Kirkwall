33ed2173063c5e99253061b94b32cc4e2fad7d4a
Riley DuBord <thelifeofdu@gmail.com>
2024-08-26 15:19:56 -0500

    temp email check for prod

--- src/Backend/ContactForm/SendEmail.mjs
+++ src/Backend/ContactForm/SendEmail.mjs
@@ -34,7 +34,7 @@ app.post('/send-enquiry', upload.array('attachments', 10), async (req, res) => {
     }));
 
     const msg = {
-        to: 'ujj.code@gmail.com',
+        to: 'thelifeofdu@gmail.com',
         from: {
             name: 'Contact Form',
             email: 'alerts@kirkwall.io'
