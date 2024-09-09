0c28de6bb9d71bfeb96aee9c49c9aff2e66c6c64
Riley DuBord <thelifeofdu@gmail.com>
2024-09-09 15:32:56 -0500

    debugging

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
     ? 'https://kirkwall-demo.vercel.app/' // Replace with your production URL
-    : 'http://localhost:3000',
+    : 'https://kirkwall-demo.vercel.app/',
 });
 
 export default api;
