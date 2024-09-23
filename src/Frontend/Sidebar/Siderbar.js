d0f91301e8fa2b636994b4611ee226e3d3ff947b
Riley DuBord <thelifeofdu@gmail.com>
2024-09-23 11:22:48 -0500

    formatting

--- src/Frontend/Sidebar/Siderbar.js
+++ src/Frontend/Sidebar/Siderbar.js
@@ -21,7 +21,7 @@ import {
   FaBook,
   FaBookOpen,
 } from 'react-icons/fa';
-import { MdElectricBolt } from "react-icons/md";
+import { MdElectricBolt } from 'react-icons/md';
 
 import { GiGroundSprout } from 'react-icons/gi';
 import { motion } from 'framer-motion';
@@ -77,14 +77,12 @@ const buttonConfig = {
     { icon: <WiRain size="30" />, label: 'Rain', route: '/RainSensors' },
     // { icon: <FaBookOpen size="30" />, label: 'Summary', route: '/summary' },
     { icon: <FaGlobe size="30" />, label: 'Map', route: '/grandfarm/map' },
-    { icon: <MdElectricBolt size="30"/>, label: 'Energy', route: '/energy' },
-
+    { icon: <MdElectricBolt size="30" />, label: 'Energy', route: '/energy' },
   ],
   'jerrycromarty@imprimedicine.com': [
     { icon: <FaBookOpen size="30" />, label: 'Summary', route: '/summary' },
     { icon: <FaGlobe size="30" />, label: 'Map', route: '/imprimed/map' },
-    { icon: <MdElectricBolt size="30"/>, label: 'Energy', route: '/energy' },
-
+    { icon: <MdElectricBolt size="30" />, label: 'Energy', route: '/energy' },
   ],
   'russell@rjenergysolutions.com': [
     {
@@ -94,8 +92,7 @@ const buttonConfig = {
     },
     { icon: <FaBookOpen size="30" />, label: 'Summary', route: '/summary' },
     { icon: <FaGlobe size="30" />, label: 'Map', route: '/rjenergy/map' },
-    { icon: <MdElectricBolt size="30"/>, label: 'Energy', route: '/energy' },
-
+    { icon: <MdElectricBolt size="30" />, label: 'Energy', route: '/energy' },
   ],
   'trey@watchdogprotect.com': [
     { icon: <FaDog size="30" />, label: 'Watchdog', route: '/WatchdogSensors' },
@@ -105,8 +102,7 @@ const buttonConfig = {
       label: 'Map',
       route: '/watchdogprotect/map',
     },
-    { icon: <MdElectricBolt size="30"/>, label: 'Energy', route: '/energy' },
-
+    { icon: <MdElectricBolt size="30" />, label: 'Energy', route: '/energy' },
   ],
   default: [
     { icon: <FaDog size="30" />, label: 'Watchdog', route: '/WatchdogSensors' },
@@ -117,7 +113,7 @@ const buttonConfig = {
     },
     { icon: <FaBookOpen size="30" />, label: 'Summary', route: '/summary' },
     { icon: <FaGlobe size="30" />, label: 'Map', route: '/map' },
-    { icon: <MdElectricBolt size="30"/>, label: 'Energy', route: '/energy' },
+    { icon: <MdElectricBolt size="30" />, label: 'Energy', route: '/energy' },
   ],
 };
 
@@ -251,7 +247,6 @@ const SidebarContent = ({ buttons, navigate }) => (
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         _hover={{ bg: '#3D5A80', color: 'white' }}
-
       >
         {btn.label}
       </MotionButton>
