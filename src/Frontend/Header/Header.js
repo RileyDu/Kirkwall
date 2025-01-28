a001458e474e1017e9ee6ee2ea8b15a98530971d
Riley DuBord <thelifeofdu@gmail.com>
2025-01-28 15:17:10 -0600

    removed unused summary button & button

--- src/Frontend/Header/Header.js
+++ src/Frontend/Header/Header.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { MdElectricBolt } from 'react-icons/md';
-import SummaryButton from '../SummaryComponent/SummaryButton.js';
 import {
   useMediaQuery,
   Flex,
