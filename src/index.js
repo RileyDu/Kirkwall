b4347352ff5de0a22ad0d6e39ad7a4d11773916c
Riley DuBord <thelifeofdu@gmail.com>
2025-03-04 13:35:03 -0600

    trying it in the head of the project vs npm for prod

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App.js';
 import Clarity from '@microsoft/clarity'
 
-const projectID = 'qj0yab72ei';
-Clarity.init(projectID);
+// const projectID = 'qj0yab72ei';
+// Clarity.init(projectID);
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
